e47445e4899e96abd837b6afb977c8bcfde68def
Peter Sideris <petersid2022@gmail.com>
2024-06-01 19:50:07 +0300

    duthweatherstation v1.7 added navbar and slight appearance fixups

--- cmd/web/base_templ.go
+++ cmd/web/base_templ.go
@@ -23,7 +23,7 @@ func Base() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><meta http-equiv=\"refresh\" content=\"120\"><title>DUTh Weather Station</title><script src=\"assets/js/htmx.min.js\"></script><script src=\"https://cdnjs.cloudflare.com/ajax/libs/d3/7.9.0/d3.min.js\"></script><script src=\"https://cdn.tailwindcss.com\"></script></head><body><h1 class=\"py-0.5 text-3xl leading-none tracking-tight text-gray-900 text-center\">DUTh Weather Station</h1><main>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><meta http-equiv=\"refresh\" content=\"120\"><title>DUTh Weather Station</title><script src=\"assets/js/htmx.min.js\"></script><script src=\"https://cdnjs.cloudflare.com/ajax/libs/d3/7.9.0/d3.min.js\"></script><script src=\"https://cdn.tailwindcss.com\"></script></head><body><main>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
