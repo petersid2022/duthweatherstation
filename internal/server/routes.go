e6cfcfae7ba5151f0361b48b0a38f096b42d25da
Peter Sideris <petersid2022@gmail.com>
2024-06-03 00:20:00 +0300

    duthweatherstation v1.8 added favicon

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -17,6 +17,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 	mux.Handle("/", templ.Handler(web.Chart()))
 	mux.HandleFunc("/api/data", s.ChartDataHandler)
 	mux.HandleFunc("/api/add", s.AddSensorDataHandler)
+	mux.HandleFunc("/favicon.ico", s.FaviconHandler)
 
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
@@ -27,6 +28,10 @@ func (s *Server) RegisterRoutes() http.Handler {
 	})
 
 	return c.Handler(mux)
+}
+
+func (s *Server) FaviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "favicon.ico")
 }
 
 func (s *Server) AddSensorDataHandler(w http.ResponseWriter, r *http.Request) {
